c8c595f0664360d84f5b2cfc91cb2d41a665420d
Camilo Nieto <camilo.nieto@gmail.com>
2018-10-02 11:13:34 +0200

    added the 'Logical negation comparisson' TestSuite

--- src/test/java/BaseFunctionality.java
+++ src/test/java/BaseFunctionality.java
@@ -4,11 +4,19 @@ import org.testng.annotations.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotEquals;
 
 @Slf4j
-public class FailureReasons {
+public class BaseFunctionality {
 
-    @Test()
+    private static final String A = "A";
+    private static final String A_1 = "A";
+    private static final String B = "B";
+
+    @Test
     public void assertTrueTest() {
         boolean isElementPresent = false;
 
@@ -16,7 +24,7 @@ public class FailureReasons {
         Assert.assertTrue(isElementPresent);
     }
 
-    @Test()
+    @Test
     public void assertTrueHamcrestTest() {
         boolean isElementPresent = false;
 
@@ -28,7 +36,7 @@ public class FailureReasons {
      * A meaningful assertion failed message will greatly help debugging and tracing
      * Note that TestNG concatenates your message to the base string returned by the assertion method
      */
-    @Test()
+    @Test
     public void assertTrueWithMessageTest() {
         boolean isElementPresent = false;
 
@@ -42,7 +50,7 @@ public class FailureReasons {
     /**
      * Hamcrest assertions display the exact error message provided by the tester
      */
-    @Test()
+    @Test
     public void assertTrueWithMessageHamcrestTest() {
         boolean isElementPresent = false;
 
@@ -51,6 +59,27 @@ public class FailureReasons {
                 getElementNotPresentAssertionMessage(isElementPresent),
                 isElementPresent, equalTo(true)
         );
+    }
+
+    @Test
+    public void logicalNegationTestNGTest() {
+        log.info("Logical negation using TestNG's 'assertNotEquals': ");
+        assertNotEquals(A, B);
+        assertNotEquals(A, A_1);
+    }
+
+    @Test
+    public void logicalNegationAssertFalseTestNGTest() {
+        log.info("Logical negation using TestNG's 'assertFalse': ");
+        assertFalse(A.equals(B));
+        assertFalse(A.equals(A_1));
+    }
+
+    @Test
+    public void logicalNegationHamcrestTest() {
+        log.info("Logical negation using Hamcrest: ");
+        assertThat(A, is(not(equalTo(B))));
+        assertThat(A, not(equalTo(A_1)));
     }
 
     private String getElementNotPresentAssertionMessage(boolean isElementPresent) {
