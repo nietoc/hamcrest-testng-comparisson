fc547f45302bc37dba2a124cc92c008c58f29c18
Camilo Nieto <camilo.nieto@gmail.com>
2018-10-02 17:24:47 +0200

    added a comparisson for the both Matcher

--- src/test/java/AllOfMatcher.java
+++ src/test/java/AllOfMatcher.java
@@ -2,29 +2,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.both;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.number.OrderingComparison.greaterThan;
 import static org.hamcrest.number.OrderingComparison.lessThanOrEqualTo;
 
 @Slf4j
-public class CoreHamcrestMatchers {
+public class AllOfMatcher {
 
     @Test
-    public void allOfGreaterThanTestA() {
+    public void allOfMatcheTestA() {
         assertNumberBetweenRange(5, 0, 10);
         assertNumberBetweenRange(5, 4, 5);
-        log.info("5 is not greater than 5");
+        log.info("5 is not greater than 5 - using 'assertNumberBetweenRange'");
         assertNumberBetweenRange(5, 5, 5);
     }
 
     @Test
-    public void allOfGreaterThanTestB() {
+    public void allOfMatcheTestB() {
         log.info("5 is less than 5");
         assertNumberBetweenRange(5, 10, 20);
     }
 
     @Test
-    public void allOfLessThanTestB() {
+    public void allOfMatcheTestC() {
         log.info("30 is greater than 20");
         assertNumberBetweenRange(30, 10, 20);
     }
@@ -33,9 +34,17 @@ public class CoreHamcrestMatchers {
      * Note that Hamcrest 'short circuits' (fails fast) and stops checking as soon as one matcher is not met
      */
     @Test
-    public void allOfBothConditionsFail() {
+    public void allOfNeitherIsMetTest() {
         log.info("150 is neither greater than 199 nor lesser than 100");
         assertNumberBetweenRange(1, 199, 100);
+    }
+
+    @Test
+    public void bothMatcherTest() {
+        assertNumberBetweenRangeUsingBothMatcher(5, 0, 10);
+        assertNumberBetweenRangeUsingBothMatcher(5, 4, 5);
+        log.info("5 is not greater than 5 - using 'assertNumberBetweenRangeUsingBothMatcher'");
+        assertNumberBetweenRangeUsingBothMatcher(5, 5, 5);
     }
 
     /**
@@ -48,6 +57,17 @@ public class CoreHamcrestMatchers {
                         greaterThan(minExclusive),
                         lessThanOrEqualTo(maxInclusive)
                 )
+        );
+    }
+
+    /**
+     * Same functionality as in {@link AllOfMatcher#assertNumberBetweenRange(int, int, int)}
+     */
+    private void assertNumberBetweenRangeUsingBothMatcher(int input, int minExclusive, int maxInclusive) {
+        assertThat(
+                input,
+                both(greaterThan(minExclusive))
+                        .and(lessThanOrEqualTo(maxInclusive))
         );
     }
 }
