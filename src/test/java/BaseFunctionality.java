68a39d9dedbef453587ed6529655c9c9d5382d4c
Camilo Nieto <camilo.nieto@gmail.com>
2018-10-02 11:36:32 +0200

    improvements in the BaseFunctionality code examples

--- src/test/java/BaseFunctionality.java
+++ src/test/java/BaseFunctionality.java
@@ -16,20 +16,21 @@ public class BaseFunctionality {
     private static final String A_1 = "A";
     private static final String B = "B";
 
-    @Test
-    public void assertTrueTest() {
-        boolean isElementPresent = false;
+    private static final int ACTUAL_VALUE = 1;
+    private static final int EXPECTED_VALUE = 2;
 
+    @Test
+    public void assertTrueTestNgTest() {
         log.info("TestNG's assertTrue without a message:");
-        Assert.assertTrue(isElementPresent);
+        Assert.assertTrue(A.equals(A_1));
+        Assert.assertTrue(A.equals(B));
     }
 
     @Test
-    public void assertTrueHamcrestTest() {
-        boolean isElementPresent = false;
-
-        log.info("Hamcrest's 'assertTrue' without a message:");
-        assertThat(isElementPresent, equalTo(true));
+    public void equalToHamcrestTest() {
+        log.info("Hamcrest's 'equalTo' without a message:");
+        assertThat(A, equalTo(A_1));
+        assertThat(A, equalTo(B));
     }
 
     /**
@@ -37,39 +38,39 @@ public class BaseFunctionality {
      * Note that TestNG concatenates your message to the base string returned by the assertion method
      */
     @Test
-    public void assertTrueWithMessageTest() {
-        boolean isElementPresent = false;
-
-        log.info("TestNG's assertTrue with message:");
-        Assert.assertTrue(
-                isElementPresent,
-                getElementNotPresentAssertionMessage(isElementPresent)
+    public void assertEqualsWithMessageTestNGTest() {
+        log.info("TestNG's assertEquals with message:");
+        Assert.assertEquals(
+                ACTUAL_VALUE, EXPECTED_VALUE,
+                getValueMismatchAssertionMessage(ACTUAL_VALUE, EXPECTED_VALUE)
         );
     }
 
     /**
      * Hamcrest assertions display the exact error message provided by the tester
+     * Note that the position for the message is opposite to TestNG's
      */
     @Test
-    public void assertTrueWithMessageHamcrestTest() {
-        boolean isElementPresent = false;
-
+    public void equalToWithMessageHamcrestTest() {
         log.info("Hamcrest's 'assertTrue' with a message:");
         assertThat(
-                getElementNotPresentAssertionMessage(isElementPresent),
-                isElementPresent, equalTo(true)
+                getValueMismatchAssertionMessage(ACTUAL_VALUE, EXPECTED_VALUE),
+                ACTUAL_VALUE, equalTo(EXPECTED_VALUE)
         );
     }
 
+    /**
+     * Weirdly enough all that is outputted by TestNG is 'java.lang.AssertionError: null'
+     */
     @Test
-    public void logicalNegationTestNGTest() {
+    public void logicalNegationTestNgTest() {
         log.info("Logical negation using TestNG's 'assertNotEquals': ");
         assertNotEquals(A, B);
         assertNotEquals(A, A_1);
     }
 
     @Test
-    public void logicalNegationAssertFalseTestNGTest() {
+    public void logicalNegationAssertFalseTestNgTest() {
         log.info("Logical negation using TestNG's 'assertFalse': ");
         assertFalse(A.equals(B));
         assertFalse(A.equals(A_1));
@@ -82,7 +83,7 @@ public class BaseFunctionality {
         assertThat(A, not(equalTo(A_1)));
     }
 
-    private String getElementNotPresentAssertionMessage(boolean isElementPresent) {
-        return String.format("Expected the element to be present but got %s", isElementPresent);
+    private String getValueMismatchAssertionMessage(int expected, int actual) {
+        return String.format("The provided values do not match! Expected %s got %s", expected, actual);
     }
 }
